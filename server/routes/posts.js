f9e186c3e2ab0f64933d1d0d3727e297bd25fe6b
aim-t <namiatariq@gmail.com>
2023-06-19 18:41:46 +0500

    Format router

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -5,11 +5,7 @@ import { verifyToken } from "../middleware/auth.js";
 const router = express.Router();
 
 // READ
-router.get(
-  "/",
-  verifyToken,
-  getFeedPosts
-); /*This will grab the user feed when we are on the homepage. It will take evry single pst in the DB and how it to us. Usually we have algos for that, but to keep it simple, we'll just show all posts */
+router.get("/",verifyToken, getFeedPosts); /*This will grab the user feed when we are on the homepage. It will take evry single pst in the DB and how it to us. Usually we have algos for that, but to keep it simple, we'll just show all posts */
 router.get("/:userId/posts", verifyToken, getUserPosts); /* We want to grab only the relevant user's posts. eg we go to a profile and see their posts only */
 
 
