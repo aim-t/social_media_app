a0ddb720a9baee1febef13b4cb6b0cd521d27e0d
aim-t <namiatariq@gmail.com>
2023-06-22 19:52:17 +0500

    Set up Redux Toolkit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,20 @@
+import { BrowserRouter, Navigate, Routes, Route } from "react-router-dom";
+import Homepage from "scenes/homePage/Homepage";
+import LoginPage from "scenes/loginPage/LoginPage";
+import ProfilePage from "scenes/profilePage/ProfilePage";
+
 function App() {
 
   return (
-    <>
-      App! 
-    </>
+    <div className="app">
+      <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<LoginPage />} /> 
+        <Route path="/home" element={<Homepage />} /> 
+        <Route path="/profile/:userId" element={<ProfilePage />} /> 
+      </Routes>
+      </BrowserRouter>
+    </div>
   )
 }
 
