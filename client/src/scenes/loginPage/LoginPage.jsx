1347965db1e655fa226c6acdaa5e2b771cb84689
aim-t <namiatariq@gmail.com>
2023-07-15 19:09:36 +0500

    complete login and signup forms

--- client/src/scenes/loginPage/LoginPage.jsx
+++ client/src/scenes/loginPage/LoginPage.jsx
@@ -1,8 +1,45 @@
+import { Box, Typography, useTheme, useMediaQuery } from "@mui/material";
+import Form from "./Form";
+
 function LoginPage() {
-    return (
-        <div>
-            LoginPage
-        </div>
-    )
+  const theme = useTheme();
+  const isNonMobileScreens = useMediaQuery("(min-width: 1000px)");
+
+  return (
+    <Box pb="2rem">
+      <Box
+        width="100%"
+        backgroundColor={theme.palette.background.alt}
+        p="1rem 6%"
+        textAlign="center"
+      >
+        <Typography
+          fontWeight="bold"
+          fontSize="32px"
+          color="primary"
+        >
+          LifeStory
+        </Typography>
+      </Box>
+
+      <Box
+        width={isNonMobileScreens ? "35%" : "93%"}
+        p="2rem"
+        m="2rem auto"
+        borderRadius="1.5rem"
+        backgroundColor={theme.palette.background.alt}
+      >
+        <Typography
+          fontWeight="500"
+          variant="h5"
+          sx={{ mb: "1.5rem", textAlign: "center" }}
+        >
+          Share your life, one story at a time!
+        </Typography>
+        <Form />
+      </Box>
+    </Box>
+  );
 }
 export default LoginPage;
+
